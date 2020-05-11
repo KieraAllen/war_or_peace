99e66851089b764ed2ed579a283468e3e772fa0b
Kiera Allen <kieraaallen@gmail.com>
2020-05-10 20:44:49 -0600

    Test spoils go to war turn winner

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -88,18 +88,26 @@ class TurnTest < Minitest::Test
       winner = turn.winner
       assert_equal player2, winner
     end
+
+    def test_it_can_award_spoils_to_war_turn_winner
+      deck1 = Deck.new([@card1, @card2, @card5, @card8])
+      deck2 = Deck.new([@card4, @card3, @card6, @card7])
+
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+
+      turn = Turn.new(player1, player2)
+
+      winner = turn.winner
+      turn.pile_cards
+      turn.spoils_of_war
+      turn.award_spoils(winner)
+      assert_equal deck1, player1.deck
+      assert_equal deck2, player2.deck
+    end
 end
 
-# pry(main)> winner = turn.winner
-# #=> #<Player:0x007fc42aab6b70 @deck=#<Deck:0x007fc42a248678...>, @name="Aurora">
-#
-# pry(main)> turn.pile_cards
-#
-# pry(main)> turn.spoils_of_war
-# #=> [#<Card:0x007fc42a170fe8...>, #<Card:0x007fc42a0f1b58...>, #<Card:0x007fc42a2149e0...>, #<Card:0x007fc42a205508...>, #<Card:0x007fc42aa85a98...>, #<Card:0x007fc42a1e4790...>]
-#
-# pry(main)> turn.award_spoils(winner)
-#
+
 # pry(main)> player1.deck
 # #=> #<Deck:0x007fc42a05a258 @cards=[#<Card:0x007fc42a1580d8...>]>
 #
